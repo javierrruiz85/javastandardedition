232334ae500ffe98ea939bf325d0639e701f8b64
javierruiz85 <javier.ruiz.bilbao@gmail.com>
2020-03-04 08:42:17 +0100

    cambios 8:42

--- src/com/ipartek/formacion/clases/ArrayPaisesPrueba.java
+++ src/com/ipartek/formacion/clases/ArrayPaisesPrueba.java
@@ -2,60 +2,43 @@ package com.ipartek.formacion.clases;
 
 import java.util.Scanner;
 
+// utiliza la clase "Pais"
+
 public class ArrayPaisesPrueba {
 
 	public static void main(String[] args) {
 
 		int totalCasos = 0;
+		Scanner teclado = new Scanner(System.in);
 
 		// declarar un Array
-		ArrayPaises[] paises = new ArrayPaises[5];
-		/*
-		 * ArrayPaises p = new ArrayPaises(); p.setNombre("España"); p.setNumCasos(120);
-		 * paises[0] = p;
-		 * 
-		 * p = new ArrayPaises(); p.setNombre("Francia"); p.setNumCasos(14); paises[1] =
-		 * p;
-		 * 
-		 * p = new ArrayPaises(); p.setNombre("Alemania"); p.setNumCasos(0); paises[2] =
-		 * p;
-		 * 
-		 * p = new ArrayPaises(); p.setNombre("Noruega"); p.setNumCasos(2); paises[3] =
-		 * p;
-		 * 
-		 * p = new ArrayPaises(); p.setNombre("Italia"); p.setNumCasos(400); paises[4] =
-		 * p;
-		 */
+		Pais[] paises = new Pais[5];
 
 		for (int i = 0; i < paises.length; i++) {
 
 			System.out.println("Dame el nombre de un pais: ");
-			Scanner teclado = new Scanner(System.in);
+
 			String nombrePais = teclado.nextLine();
 
 			System.out.println("Dame el total de infectados de ese pais: ");
-			int totalInfectados = teclado.nextInt();
+			String totalInfectados = teclado.nextLine();
+			int parse_totalInfectados = Integer.parseInt(totalInfectados);
 
-			ArrayPaises p = new ArrayPaises();
+			Pais p = new Pais();
 			p.setNombre(nombrePais);
-			p.setNumCasos(totalInfectados);
+			p.setNumeroInfectados(parse_totalInfectados);
 			paises[i] = p;
-
-			teclado.close();
 
 		}
 
 		for (int i = 0; i < paises.length; i++) {
-			// System.out.println("En " + paises[i].getNombre() + " hay " +
-			// paises[i].getNumCasos() + " infectados");
+			totalCasos = totalCasos + paises[i].getNumeroInfectados();
 
-			totalCasos = totalCasos + paises[i].getNumCasos();
-
-			if (paises[i].getNumCasos() > 0) {
-				System.out.println("En " + paises[i].getNombre() + " hay " + paises[i].getNumCasos() + " infectados");
+			if (paises[i].getNumeroInfectados() > 0) {
+				System.out.println(
+						"En " + paises[i].getNombre() + " hay " + paises[i].getNumeroInfectados() + " infectados");
 			} else {
 				System.out.println("En " + paises[i].getNombre() + " no hay infectados");
-
 			}
 
 		}
@@ -67,6 +50,8 @@ public class ArrayPaisesPrueba {
 		 * 
 		 * } }
 		 */
+
+		teclado.close();
 
 	}
 
