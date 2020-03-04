cb238eb7d5eaa1ae1105c2f86eabee10ae6bbc40
javierruiz85 <javier.ruiz.bilbao@gmail.com>
2020-03-04 14:01:04 +0100

    14:00

---
+++ src/prueba/EjercicioA.java
@@ -0,0 +1,53 @@
+package prueba;
+
+import java.util.Scanner;
+
+/**
+ *
+ * array con 5 alturas de personas (float, double o int). el array se rellena
+ * pidiendo los datos por teclado. una vez rellenado, calcular la altura media,
+ * la mas alta y la mas baja
+ * 
+ * @author Curso
+ *
+ */
+
+public class EjercicioA {
+
+	public static void main(String[] args) {
+
+		float[] alturas = new float[5];
+		float total = 0;
+		float mayor = 0;
+		float menor = 0;
+
+		Scanner escaner = new Scanner(System.in);
+
+		for (int i = 0; i < alturas.length; i++) {
+			System.out.println("Dame una altura: ");
+			int altura = Integer.parseInt(escaner.nextLine());
+			alturas[i] = altura;
+			total = total + alturas[i];
+
+			// TODO revisar esta mierda que no rula
+			mayor = altura;
+			menor = altura;
+
+			if (altura >= mayor) {
+				mayor = altura;
+			}
+
+			if (altura <= menor) {
+				menor = altura;
+			}
+		}
+
+		System.out.println("La altura media es de : " + total / 5);
+		System.out.println("La altura mayor es " + mayor);
+		System.out.println("La altura mayor es " + menor);
+
+		escaner.close();
+
+	}
+
+}
---
+++ src/prueba/EjercicioB.java
@@ -0,0 +1,29 @@
+package prueba;
+
+/**
+ * 
+ *crear proyecto en modo consola, un menu con 3 opciones
+ *calcular la letra del dni
+ *calcular medida de grados a farenheit
+ *salir del menu
+ *
+ *
+ *BUCLES
+ *
+ *FOR: hacer cuando haya una coleccion de datos
+ *WHILE: intentar evitar
+ *DO WHILE: usar cuando se tenga que ejecutar la repeticion al menos una vez
+ *
+ *
+ * @author Curso
+ *
+ */
+
+public class EjercicioB {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
